94fdfa59c254e28166e7babde7d729f93251b2bc
scal3 <cool.goga501@gmail.com>
2022-01-17 16:46:10 +0300

    feat/add socket.io

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const express = require('express')
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+  })
 
-const { PORT = 3000 } = process.env;
-const app = express()
+const { PORT = 9999 } = process.env
 
 const rooms = new Map([
-    
+
 ])
 
-app.get('/', (req, res) => {
-    res.json(rooms)
+
+app.get('/rooms', (req, res) => {
+    res.json('sasi')
 })
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+    console.log('user connected', socket.id)
+})
+
+server.listen(PORT, (err) => {
+    if(err) {
+        throw new Error(err)
+    }
     console.log(`App listening on port ${PORT}`)
 }) 
